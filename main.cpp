07b9152b537a4cc9a24f2db96a374d4fd8f50ad5
dehwyy <dehwyy@yandex.ru>
2024-09-11 12:31:05 +0300

    constant name fix

--- main.cpp
+++ main.cpp
@@ -1,34 +1,34 @@
 #include <iostream>
 
+const int kMaxHours = 24;
+const int kDaybreakHours = 12;
+const int kMaxMinutes = 60;
+
+const int kMorningStartHours = 5;
+const int kMorningEndHours = 12;
+const int kDayStartHours = 12;
+const int kDayEndHours = 18;
+const int kAfternoonStartHours = 18;
+const int kAfternoonEndHours = 23;
+
+const int kDecimalBase = 10;
+const int kDigit2 = 2;
+const int kDigit3 = 3;
+const int kDigit4 = 4;
+
 int main() {
-    const int MAX_HOURS = 24;
-    const int DAYBREAK_HOURS = 12;
-    const int MAX_MINUTES = 60;
-
-    const int MORNING_START_HOURS = 5;
-    const int MORNING_END_HOURS = 12;
-    const int DAY_START_HOURS = 12;
-    const int DAY_END_HOURS = 18;
-    const int AFTERNOON_START_HOURS = 18;
-    const int AFTERNOON_END_HOURS = 23;
-
-    const int DECIMAL_DIGIT = 10;
-    const int DIGIT_2 = 2;
-    const int DIGIT_3 = 3;
-    const int DIGIT_4 = 4;
-
     int hours = 0;
     int minutes = 0;
     int hoursFormated = 0;
     std::cin >> hours;
     std::cin >> minutes;
 
-    if (hours < 0 || hours >= MAX_HOURS || minutes < 0 || minutes > MAX_MINUTES) {
+    if (hours < 0 || hours >= kMaxHours || minutes < 0 || minutes > kMaxMinutes) {
         std::cout << "Введены недопустимые данные;" << std::endl;
         return 1;
     }
 
-    if (minutes == 0 && (hours == 0 || hours == DAYBREAK_HOURS)) {
+    if (minutes == 0 && (hours == 0 || hours == kDaybreakHours)) {
         if (hours == 0) {
             std::cout << "полночь;" << std::endl;
         } else {
@@ -37,8 +37,8 @@ int main() {
         return 0;
     }
 
-    if (hours > DAYBREAK_HOURS) {
-        hoursFormated = hours - DAYBREAK_HOURS;
+    if (hours > kDaybreakHours) {
+        hoursFormated = hours - kDaybreakHours;
     } else {
         hoursFormated = hours;
     }
@@ -47,7 +47,7 @@ int main() {
 
     if (hoursFormated == 1) {
         std::cout << "час ";
-    } else if (hoursFormated == DIGIT_2 || hoursFormated == DIGIT_3 || hoursFormated == DIGIT_4) {
+    } else if (hoursFormated == kDigit2 || hoursFormated == kDigit3 || hoursFormated == kDigit4) {
         std::cout << "часа ";
     } else {
         std::cout << "часов ";
@@ -55,21 +55,21 @@ int main() {
 
     if (minutes != 0) {
         std::cout << minutes << " ";
-        if (minutes == 1 || (minutes % DECIMAL_DIGIT == 1 && minutes % DECIMAL_DIGIT != 1)) {
+        if (minutes == 1 || (minutes % kDecimalBase == 1 && minutes % kDecimalBase != 1)) {
             std::cout << "минута ";
-        } else if ((minutes % DECIMAL_DIGIT == DIGIT_2 || minutes % DECIMAL_DIGIT == DIGIT_3 || minutes % DECIMAL_DIGIT == DIGIT_4) &&
-                   minutes / DECIMAL_DIGIT != 1) {
+        } else if ((minutes % kDecimalBase == kDigit2 || minutes % kDecimalBase == kDigit3 || minutes % kDecimalBase == kDigit4) &&
+                   minutes / kDecimalBase != 1) {
             std::cout << "минуты ";
         } else {
             std::cout << "минут ";
         }
     }
 
-    if (hours >= MORNING_START_HOURS && hours < MORNING_END_HOURS) {
+    if (hours >= kMorningStartHours && hours < kMorningEndHours) {
         std::cout << "утра";
-    } else if (hours > DAY_START_HOURS && hours < DAY_END_HOURS) {
+    } else if (hours >= kDayStartHours && hours < kDayEndHours) {
         std::cout << "дня";
-    } else if (hours >= AFTERNOON_START_HOURS && hours < AFTERNOON_END_HOURS) {
+    } else if (hours >= kAfternoonStartHours && hours < kAfternoonEndHours) {
         std::cout << "вечера";
     } else {
         std::cout << "ночи";
